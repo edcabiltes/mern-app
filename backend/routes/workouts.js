f6316fe816262b2b93ee798649e83748324ae220
Ed Cabiltes <edcabiltes@outlook.com>
2025-02-16 20:17:15 +0800

    Implemented controllers

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,31 +1,28 @@
 const express = require(`express`)
-const Workout = require(`../models/WorkoutModel`)
+const {
+    getWorkouts,
+    getWorkout,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require(`../controllers/workoutController`)
 
 const router = express.Router()
 
 // GET all workouts
-router.get(`/`, (req, res) => {res.json({mssg: `GET all workouts`})})
+router.get(`/`, getWorkouts)
 
 // GET a single workout
-router.get(`/:id`, (req, res) => {res.json({mssg: `GET a single workout`})})
+router.get(`/:id`, getWorkout)
 
 // POST a new workout
-router.post(`/`, async (req, res) => {
-    const {name, weight, sets, reps} = req.body
-
-    try{
-        const workout = await Workout.create({name, weight, sets, reps})
-        res.status(200).json(workout)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-})
+router.post(`/`, createWorkout)
 
 // DELETE a workout
-router.delete(`/:id`, (req, res) => {res.json({mssg: `DELETE a workout`})})
+router.delete(`/:id`, deleteWorkout)
 
 // UPDATE a workout
-router.patch(`/:id`, (req, res) => {res.json({mssg: `UPDATE a workout`})})
+router.patch(`/:id`, updateWorkout)
 
 module.exports = router
 
