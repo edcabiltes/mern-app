285875444b4f895593c9dd7538ee509ed91a51df
Ed Cabiltes <edcabiltes@outlook.com>
2025-02-16 15:36:08 +0800

    Defined models and schemas

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require(`express`)
+const Workout = require(`../models/WorkoutModel`)
 
 const router = express.Router()
 
@@ -9,7 +10,16 @@ router.get(`/`, (req, res) => {res.json({mssg: `GET all workouts`})})
 router.get(`/:id`, (req, res) => {res.json({mssg: `GET a single workout`})})
 
 // POST a new workout
-router.post(`/`, (req, res) => {res.json({mssg: `POST a new workout`})})
+router.post(`/`, async (req, res) => {
+    const {name, weight, sets, reps} = req.body
+
+    try{
+        const workout = await Workout.create({name, weight, sets, reps})
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+})
 
 // DELETE a workout
 router.delete(`/:id`, (req, res) => {res.json({mssg: `DELETE a workout`})})
